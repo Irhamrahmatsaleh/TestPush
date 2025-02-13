69654a0c55a67c1fba28c4d92b1f4324339b6996
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-02-13 12:27:06 +0700

    inverted pyramid

--- assesment/FCC/pyramid.js
+++ assesment/FCC/pyramid.js
@@ -15,8 +15,12 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }*/
 
-while (rows.length < count) {
+/*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
+}*/
+
+for (let i = count; i > 0; i = i - 1) {
+  row.push(padRow(i, count));
 }
 
 let result = '';
