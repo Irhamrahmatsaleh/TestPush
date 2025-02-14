ff83bae75c3546b883cf940927d9919be58a10d4
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-02-14 10:26:54 +0700

    add pyramid generation functionality with customizable character and inverted option

--- test-logika.js
+++ test-logika.js
@@ -7,3 +7,34 @@ function hitungTotal(array) {
 }
 
 console.log(hitungTotal([1, 2, 3, 4, 5]));
+
+//--------------------------------------------
+
+const character = '#';
+const count = 8;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return (
+    ' '.repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    ' '.repeat(rowCount - rowNumber)
+  );
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = '';
+
+for (const row of rows) {
+  result += row + '\n';
+}
+
+console.log(result);
