e345f1da953e3ba5163ce2f889aae61d3268ab4b
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-02-14 12:19:22 +0700

    add getGrade function to GradeBookApp.js for letter grade calculation; refactor getAverage function for clarity

--- assesment/FCC/GradeBookApp.js
+++ assesment/FCC/GradeBookApp.js
@@ -1,11 +1,5 @@
-// function getAverage(scores) {
-//   let total = 0;
-//   for (let i = 0; i < scores.length; i++) {
-//     total += scores[i];
-//   }
-//   return total / scores.length;
-// }
-
+//-------------------GradeBookApp.js-------------------
+// 1. Create a function named getAverage that takes an array of scores as an argument and returns the average score.
 function getAverage(scores) {
   let sum = 0;
   for (const score of scores) {
@@ -13,6 +7,26 @@ function getAverage(scores) {
   }
   return sum / scores.length;
 }
+getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]);
+getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]);
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+// 2. Create a function named getGrade that takes a score as an argument and returns a letter grade for the score.
+function getGrade(score) {
+  if (score === 100) {
+    return 'A+';
+  } else if (score >= 90) {
+    return 'A';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
